637daf3ca306980a3656c6b2d9d1250e51bbfd2a
Min K Yoon <mkyoon713@gmail.com>
2018-09-12 10:15:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,8 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  return [array,...element];
-  return array
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
