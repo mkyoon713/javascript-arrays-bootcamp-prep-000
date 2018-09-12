7fcd52d3a229caffc7110833d5943cebe215039b
Min K Yoon <mkyoon713@gmail.com>
2018-09-12 09:27:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles")
 
+var myArray = array(1)
 function addElementToBeginningOfArray(array,element) {
  return (array.unshift(element))
 }
