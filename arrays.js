f84e5fa6ed72fc6859c4dcc14cc6ed0c3b54e120
Min K Yoon <mkyoon713@gmail.com>
2018-09-12 10:11:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice();
-  return array
+  return array.slice()
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) { array.pop();
